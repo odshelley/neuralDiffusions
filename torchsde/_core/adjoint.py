85ee77faa6d72cef0b72da0af2b19a0889f87df6
GeorgeOctavian <astravi12@gmail.com>
2024-07-26 09:45:58 +0100

    adjoint works with jumps; tbd: the other 2 vectors of adjoint

--- torchsde/_core/adjoint.py
+++ torchsde/_core/adjoint.py
@@ -15,6 +15,7 @@
 import torch
 from torch import nn
 import warnings
+# import pydevd
 
 from . import base_sde
 from . import methods
@@ -30,10 +31,13 @@ class _SdeintAdjointMethod(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, sde, ts, dt, bm, solver, method, adjoint_method, adjoint_adaptive, adjoint_rtol, adjoint_atol,
-                dt_min, adjoint_options, len_extras, y0, *extras_and_adjoint_params):
+                dt_min, adjoint_options, len_extras, 
+                jump_times, gradient_map, shapes,
+                y0, *extras_and_adjoint_params):
+        # pydevd.settrace(suspend=False, trace_only_current_thread=True)
         ctx.sde = sde
         ctx.dt = dt
-        ctx.bm = bm
+        ctx.bm = bm      
         ctx.adjoint_method = adjoint_method
         ctx.adjoint_adaptive = adjoint_adaptive
         ctx.adjoint_rtol = adjoint_rtol
@@ -42,14 +46,142 @@ class _SdeintAdjointMethod(torch.autograd.Function):
         ctx.adjoint_options = adjoint_options
         ctx.len_extras = len_extras
 
+        ctx.jump_times = jump_times
+        ctx.gradient_map = gradient_map
+
         extra_solver_state = extras_and_adjoint_params[:len_extras]
         adjoint_params = extras_and_adjoint_params[len_extras:]
 
         # This .detach() is VERY IMPORTANT. See adjoint_sde.py::AdjointSDE.get_state.
         y0 = y0.detach()
         # Necessary for the same reason
+        # extra_solver_state = tuple(x.detach() for x in extra_solver_state)
+        ys = [y0]
         extra_solver_state = tuple(x.detach() for x in extra_solver_state)
-        ys, extra_solver_state = solver.integrate(y0, ts, extra_solver_state)
+        
+
+        # NOTE: works on hopes and prayers
+        has_jumps = False
+        try:
+            has_jumps = sde._base_sde.has_jumps 
+        except: 
+            pass
+
+            
+        class_type = sde.forward_sde._base_sde.__class__.__name__ if sde.__class__.__name__ == "AdjointSDE" else sde._base_sde.__class__.__name__
+        if class_type != "_VectorField":
+            # during backprop this is equal to 
+            for i in range(1, len(ts)):
+                t0, t1 = ts[i - 1], ts[i]
+                y = ys[-1].detach()
+                extra_solver_state = tuple(x.detach() for x in extra_solver_state)
+
+                if has_jumps:
+                    batch_jump_times = sde._base_sde.jump_occurred_batch(t0, t1, y.shape[0])
+                    
+                    # TODO: change this to append the right way
+                    ctx.jump_times += batch_jump_times
+
+                    # TODO: allow for batch sizes > 0 
+                    for b in range(y.shape[0]):
+                        cur_t0 = t0 
+                        jumps = batch_jump_times[b]
+
+                        for jump_time in jumps:
+
+                            extra_solver_state = tuple(x.detach() for x in extra_solver_state)
+                            y = y.detach()
+
+                            assert t0 < jump_time
+                            assert jump_time < t1
+                            # print(f"Jump time simulated at: {jump_time} is between {t0} and {t1}")
+                            
+                            ts_temp = torch.tensor([cur_t0, jump_time], device='cuda')
+
+                            # Integrate over the current interval
+                            y, extra_solver_state = solver.integrate(y, ts_temp, extra_solver_state)
+
+                            # Apply jump magnitude function if there's a jump (NOTE: only apply magnitude to correct point)
+                            y[-1] += sde._base_sde.jump_magnitude(jump_time, y[-1])
+
+                            # you want the gradients w.r.t theta on the right (w'(t+))
+                            # STORE THEM SOMEWHERE 
+                            jump_gradient_wrt_theta = sde._base_sde.jump_gradient_wrt_theta(jump_time, y[-1])
+                            jump_gradient_wrt_time = sde._base_sde.jump_gradient_wrt_time(jump_time, y[-1])
+                            jump_gradient_wrt_output = sde._base_sde.jump_gradient(jump_time, y[-1])
+
+                            with torch.no_grad():
+                                ctx.gradient_map.append((jump_gradient_wrt_theta, jump_gradient_wrt_time, jump_gradient_wrt_output))
+
+                            # TODO: look into a nicer way of doing this
+                            # extra_solver_state = solver.init_extra_solver_state(jump_time, y[-1])
+                            cur_t0 = jump_time
+                            y = y[-1]
+
+
+                    ts_temp = torch.tensor([cur_t0, t1], device='cuda')
+                    extra_solver_state = tuple(x.detach() for x in extra_solver_state)
+                    y = y.detach()
+                    y, extra_solver_state = solver.integrate(y, ts_temp, extra_solver_state)
+
+                # means we have entered backward prop? 
+                else:
+                    cur_t0 = t0
+                    idx = 0
+                    for jumps in reversed(ctx.jump_times):
+                        # Check if the jump time is between t0 and t1
+                        # TODO: not accurate, but prevents jumping twice
+                        # TODO: need to check the indices are still consistent
+                        for jt in reversed(jumps): 
+                            jump_time = -jt.item()
+                            if t0 <= jump_time <= t1:
+                                # Execute the desired operation
+                                # print(f"Jump time recorded during forward: {jump_time} is between {t0} and {t1}")
+                                
+                                ts_temp = torch.tensor([cur_t0, jump_time], device='cuda')
+                                y, extra_solver_state = solver.integrate(y, ts_temp, extra_solver_state)
+                                # (a, b), *extra_solver_state = y, extra_solver_state
+                                (temp_x, aug_state) = y
+                                # # aug_state[0] += reversed(ctx.gradient_map)[0][0]
+
+                                # # reflatten 
+                                aug_state = misc.flat_to_shape(aug_state.squeeze(0), shapes)
+                                
+                                
+                                identity_m = torch.ones_like(aug_state[1], device='cuda')
+                                
+                                diag_ident_m = torch.diag(identity_m[0])
+                                A = diag_ident_m - torch.diag(list(reversed(ctx.gradient_map))[idx][2][0])
+
+                                X = torch.linalg.solve_triangular(A, aug_state[1][0].unsqueeze(1), upper=False)
+
+                                aug_state[1] = X.transpose(0,1)
+                                # torch.transpose(X,0,1)
+
+                                # if i != 1:
+                                aug_state = misc.flatten(aug_state)
+                                aug_state = aug_state.unsqueeze(0)  # dummy batch dimension
+
+                                # y = aug_state[0]
+                                # # y = (temp_x, aug_state)
+                                # # extra_solver_state = solver.init_extra_solver_state(jump_time, y[-1])
+                                cur_t0 = jump_time
+                                y = aug_state
+
+                                idx +=1
+
+                    ts_temp = torch.tensor([cur_t0, t1], device='cuda')
+                    y, extra_solver_state = solver.integrate(y, ts_temp, extra_solver_state)
+
+                ys.append(y[-1])
+                # extra_solver_states.append(extra_solver_state)
+        
+            ys = torch.stack(ys)
+
+        else:
+            # NOTE: not a vector field
+            ys, extra_solver_state = solver.integrate(y0, ts, extra_solver_state)
+
 
         if method == METHODS.reversible_heun and adjoint_method == METHODS.adjoint_reversible_heun:
             ctx.saved_extras_for_backward = True
@@ -59,10 +191,13 @@ class _SdeintAdjointMethod(torch.autograd.Function):
             ctx.saved_extras_for_backward = False
             extras_for_backward = ()
         ctx.save_for_backward(ys, ts, *extras_for_backward, *adjoint_params)
+        
+        
         return (ys, *extra_solver_state)
 
     @staticmethod
     def backward(ctx, grad_ys, *grad_extra_solver_state):  # noqa
+        # pydevd.settrace(suspend=False, trace_only_current_thread=True)
         ys, ts, *extras_and_adjoint_params = ctx.saved_tensors
         if ctx.saved_extras_for_backward:
             extra_solver_state = extras_and_adjoint_params[:ctx.len_extras]
@@ -95,6 +230,7 @@ class _SdeintAdjointMethod(torch.autograd.Function):
             extra_solver_state = solver.init_extra_solver_state(ts[-1], aug_state)
 
         for i in range(ys.size(0) - 1, 0, -1):
+
             (_, aug_state), *extra_solver_state = _SdeintAdjointMethod.apply(adjoint_sde,
                                                                              torch.stack([-ts[i], -ts[i - 1]]),
                                                                              ctx.dt,
@@ -108,12 +244,19 @@ class _SdeintAdjointMethod(torch.autograd.Function):
                                                                              ctx.dt_min,
                                                                              ctx.adjoint_options,
                                                                              len(extra_solver_state),
+                                                                             ctx.jump_times, 
+                                                                             ctx.gradient_map,
+                                                                             shapes,
                                                                              aug_state,
                                                                              *extra_solver_state,
                                                                              *adjoint_params)
             aug_state = misc.flat_to_shape(aug_state.squeeze(0), shapes)
+            
+            # a(t)
             aug_state[0] = ys[i - 1]
+            
             aug_state[1] = aug_state[1] + grad_ys[i - 1]
+
             if i != 1:
                 aug_state = misc.flatten(aug_state)
                 aug_state = aug_state.unsqueeze(0)  # dummy batch dimension
@@ -123,7 +266,7 @@ class _SdeintAdjointMethod(torch.autograd.Function):
         else:
             out = [aug_state[1]] + ([None] * ctx.len_extras) + aug_state[2:]
         return (
-            None, None, None, None, None, None, None, None, None, None, None, None, None, *out,
+            None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, *out,
         )
 
 
@@ -237,7 +380,8 @@ def sdeint_adjoint(sde: nn.Module,
     adjoint_params = filter(lambda x: x.requires_grad, adjoint_params)
     adjoint_method = _select_default_adjoint_method(sde, method, adjoint_method)
     adjoint_options = {} if adjoint_options is None else adjoint_options.copy()
-
+    jump_times = []
+    gradient_map = []
     # Note that all of these warnings are only applicable for reversible solvers with sdeint_adjoint; none of them
     # apply to sdeint.
     if method == METHODS.reversible_heun:
@@ -270,9 +414,17 @@ def sdeint_adjoint(sde: nn.Module,
     if extra_solver_state is None:
         extra_solver_state = solver.init_extra_solver_state(ts[0], y0)
 
+    jump_times = []
+    # NOTE: hard-coded
+    # jump_times = [torch.empty(0, device='cuda') for _ in range(y0.shape[0])] 
+
+    shapes = y0.shape
+
     ys, *extra_solver_state = _SdeintAdjointMethod.apply(
         sde, ts, dt, bm, solver, method, adjoint_method, adjoint_adaptive, adjoint_rtol, adjoint_atol, dt_min,
-        adjoint_options, len(extra_solver_state), y0, *extra_solver_state, *adjoint_params
+        adjoint_options, len(extra_solver_state)
+        , jump_times, gradient_map, shapes
+        , y0, *extra_solver_state, *adjoint_params
     )
 
     return sdeint.parse_return(y0, ys, extra_solver_state, extra, logqp)
